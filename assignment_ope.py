626ea4ecc9b17c3e86798f51f990fcb5c2aeeed9
shivani kumari <97508303+shivanikum04@users.noreply.github.com>
2022-01-25 22:44:54 +0530

    Create assignment_ope.py

---
+++ assignment_ope.py
@@ -0,0 +1,71 @@
+#set 1
+x = 2
+y = 4
+print(x)
+print(y)
+#addition
+print(x+y)
+#subtraction
+print(x-y)
+#multiplication
+print(x*y)
+#equality
+print(x==y)
+print(y/x)
+print(x>y)
+print(x<=y)
+
+#set 2
+x=6
+y=9
+print(x)
+print(y)
+#addition
+print(x+y)
+#subtraction
+print(x-y)
+#multiplication
+print(x*y)
+#equality
+print(x==y)
+#division
+print(y/x)
+print(x>y)
+print(x<=y)
+
+
+#set 3
+x=1
+y=3
+print(x)
+print(y)
+#addition
+print(x+y)
+#subtraction
+print(x-y)
+#multiplication
+print(x*y)
+#equality
+print(x==y)
+#division
+print(y/x)
+print(x>y)
+print(x<=y)
+
+#set 4
+x=8
+y=7
+print(x)
+print(y)
+#addition
+print(x+y)
+#subtraction
+print(x-y)
+#multiplication
+print(x*y)
+#equality
+print(x==y)
+#division
+print(y/x)
+print(x>y)
+print(x<=y)
